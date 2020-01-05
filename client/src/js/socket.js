622a0e206846a5655c7ed40f9fe1c6c4b6166a1a
Raj Kumar <rajkumar@mygamemate.com>
2020-01-06 03:21:50 +0530

    Project Work Video Call

---
+++ client/src/js/socket.js
@@ -0,0 +1,4 @@
+/* global SOCKET_HOST */
+import io from 'socket.io-client';
+const socket = io(SOCKET_HOST);
+export default socket;
---
+++ server/main.js
@@ -0,0 +1,5 @@
+const config = require('../config.json');
+const server = require('./lib/server');
+
+config.PORT = process.env.PORT || config.PORT;
+server.run(config);
